fd845cf16aede8790cb1794b392077cb08ac5376
juliosntts <julio0123123123@hotmail.com>
2025-03-06 15:47:56 -0300

    Added authentication and register of user

---
+++ app/src/main/java/com/juliosantos/projetowhatsapp/utils/Extensions.kt
@@ -0,0 +1,8 @@
+package com.juliosantos.projetowhatsapp.utils
+
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.showMessage( message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
